25846e06fadfe83747699fe80bc51b5dc433b0fd
MasterCoder <sparlevi18@gmail.com>
2024-02-14 16:33:11 -0800

    Firt Init, and Calcultor

---
+++ first.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 12 17:37:03 2024
+
+@author: mcode
+"""
+from tkinter import *
+root = Tk() 
+root.title("Identity card") 
+root.geometry("300x400") 
+label=Label(root)
+label['text']='hello world!';
+label.pack()
+root.mainloop()
